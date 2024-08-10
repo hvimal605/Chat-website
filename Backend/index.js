516f5af60ed2cf97a1ddda0dabbff05252e853f2
HARSH KUMAR VIMAL <hvimal605@gmail.com>
2024-08-10 21:21:31 +0530

    latest commit

--- Backend/index.js
+++ Backend/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));  
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://harshspark-chat-web.netlify.app/",
   credentials: true
 }));
 require("dotenv").config()
